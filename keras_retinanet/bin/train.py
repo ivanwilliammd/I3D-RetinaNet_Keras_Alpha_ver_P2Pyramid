b00ccb3b84f2155c035ed91d8094634248f27756
Ivan ASUSDESKTOP <ivan.harsono@binus.ac.id>
2019-04-05 15:31:09 +0700

    added P2 + anchors v1.3

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -528,7 +528,7 @@ def main(args=None):
     #     rgb_model.get_layer('Conv3d_5b_0a_1x1').output])
 
     model= retinanet(inputs=img_input_ret, num_classes=num_classes, 
-        backbone_layers=[rgb_model.get_layer('Conv3d_2c_0a_1x1').output,
+        backbone_layers=[rgb_model.get_layer('Conv3d_2c_3x3').output,
         rgb_model.get_layer('Conv3d_3c_0a_1x1').output, 
         rgb_model.get_layer('Conv3d_4f_0a_1x1').output, 
         rgb_model.get_layer('Conv3d_5b_0a_1x1').output])
